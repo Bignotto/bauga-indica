00e1d8db36cdd94a1b710294dd39d9d4e570a136
Thiago Bignotto <bignotto@gmail.com>
2023-04-19 08:37:47 -0300

    save commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
-import { FormEvent } from "react";
+import { Suwannaphum } from "next/font/google";
+import { FormEvent, useState } from "react";
 import Logo from "../components/Logo";
 
+const customFont = Suwannaphum({ subsets: ["latin"], weight: "700" });
+
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
+
   async function handleSearch(event: FormEvent) {
     event.preventDefault();
-    console.log("novo click");
+    console.log("novo click -> ", searchText);
   }
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-center">
       <Logo />
-      <form className="flex flex-row mt-6 bg-red-400 gap-3">
-        <div>
-          <input />
+      <form className="flex flex-row mt-6 bg-red-400 gap-3 w-3/4 px-4">
+        <div className="flex flex-1">
+          <input
+            className={`bg-white ${customFont.className} form-input px-4 py-4 w-full`}
+            type="text"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
+          />
         </div>
-        <div>
+        <div className="flex items-center">
           <button
-            className="rounded-full bg-blue-400"
+            className=" bg-blue-400 si"
             type="button"
             onClick={handleSearch}
           >
-            OK
+            Buscar
           </button>
         </div>
       </form>
