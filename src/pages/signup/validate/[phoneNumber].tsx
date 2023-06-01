555bfa4b5b1b570e6e69bf9af769ad4242ca6319
Thiago Bignotto <bignotto@gmail.com>
2023-06-01 06:52:48 -0300

    sign up flow implemented

--- src/pages/signup/validate/[phoneNumber].tsx
+++ src/pages/signup/validate/[phoneNumber].tsx
@@ -1,23 +1,43 @@
 import Logo from "@/components/Logo";
 import { api } from "@/services/api";
+import { AxiosError } from "axios";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function ValidateNewUserPhone() {
   const router = useRouter();
   const { phoneNumber } = router.query;
 
   const [otp, setOtp] = useState("");
+  const [errorStatus, setErrorStatus] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  async function handleVerifyOtp() {
+  async function handleVerifyOtp(e: FormEvent) {
+    e.preventDefault();
+
     if (!otp) return;
 
-    const response = await api.post("verify-code", {
-      phone: `+55${phoneNumber}`,
-      code: otp,
-    });
+    try {
+      const response = await api.post("verify-code", {
+        phone: `+55${phoneNumber}`,
+        code: otp,
+      });
 
-    console.log(response.data);
+      const updateResponse = await api.patch("users/phoneValidated", {
+        phoneNumber,
+      });
+
+      router.push("/");
+      //TODO: to decide -> redirect to home? logged dashboard? login page?
+    } catch (error) {
+      console.log({ error });
+      if (error instanceof AxiosError) {
+        setErrorMessage(error.response?.data.message);
+        setErrorStatus(error.status || error.response?.status || 0);
+        return;
+      }
+      setErrorStatus(500);
+    }
   }
 
   return (
@@ -28,7 +48,7 @@ export default function ValidateNewUserPhone() {
         </div>
         <h1>This is the OTP validation page for: {phoneNumber}</h1>
       </div>
-      <form className="flex flex-row mt-6 w-4/5" onSubmit={handleVerifyOtp}>
+      <form className="flex flex-row mt-6 w-4/5">
         <div className="flex flex-1 px-4 py-4 bg-blue-200">
           <label htmlFor="otp">
             Digite o código enviado para o seu telefone
@@ -45,11 +65,26 @@ export default function ValidateNewUserPhone() {
           <button
             type="submit"
             className={` bg-slate-600 text-white w-20 h-10`}
+            onClick={handleVerifyOtp}
           >
             Verificar
           </button>
         </div>
       </form>
+      {(errorStatus === 400 || errorStatus === 404) && (
+        <div className="flex bg-red-300 w-4/5 mt-3 px-4 py-4 justify-center">
+          {errorMessage ? (
+            <h1>{errorMessage}</h1>
+          ) : (
+            <h1>Algo errado com a verificação do seu código.</h1>
+          )}
+        </div>
+      )}
+      {errorStatus === 500 && (
+        <div className="flex bg-red-300 w-4/5 mt-3 px-4 py-4 justify-center">
+          <h1>Algo errado com a verificação do seu código.</h1>
+        </div>
+      )}
     </main>
   );
 }
