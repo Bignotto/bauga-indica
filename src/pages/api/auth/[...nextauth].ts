2a3ff75a13c154155d969a40a2ad1bfa72f7cef0
Thiago Bignotto <bignotto@gmail.com>
2023-06-12 06:30:03 -0300

    fixed user prisma model to work with NextAuth prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,10 +3,11 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@/database/prisma";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: AuthOptions = {
   secret: process.env.NextAuth_SECRET,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
